51da1bbb9ad9bf52aa006ef8b240087d716d7673
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-03-04 15:36:42 -0700

    Update convertToChars.py

--- convertToChars.py
+++ convertToChars.py
@@ -7,27 +7,28 @@ base_dir="/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1/main"
 walk=os.walk("/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1/main")
 
 dirs=[]
-for entry in walk:
-	dirs.append(entry[0])
+for x in range(len(walk)):
+	dirs.append(walk[x][0])
 	#print(entry[0])
 
+for x in range(len(dirs)):
+	print(dirs)
 
-
-for entry in dirs:
-	print(entry)
-	dirWalk=os.walk(entry)
-	for walkEntry in dirWalk:
-		files=walkEntry[2]
+# for entry in dirs:
+# 	print(entry)
+# 	dirWalk=os.walk(entry)
+# 	for walkEntry in dirWalk:
+# 		files=walkEntry[2]
 	
-	x=0
-	while x < len(files):
-		if ".txt" not in files[x]:
-			files.pop(x)
-		else:
-			x+=1
+# 	x=0
+# 	while x < len(files):
+# 		if ".txt" not in files[x]:
+# 			files.pop(x)
+# 		else:
+# 			x+=1
 
-	for file in files:
-		openable=entry+"/"+file
-		print(openable)
-		f=open(openable,"r")
-		print(f.read(0))
+# 	for file in files:
+# 		openable=entry+"/"+file
+# 		print(openable)
+# 		f=open(openable,"r")
+# 		print(f.read(0))
