4954e0ea3dfc5a2f61bb42890a39996b06287767
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-03-04 14:57:45 -0700

    Update convertToChars.py

--- convertToChars.py
+++ convertToChars.py
@@ -4,7 +4,11 @@ from tqdm import tqdm
 
 base_dir="/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1/main"
 
-dirs=os.walk("/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1/main")
+walk=os.walk("/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1/main")
+
+dirs=[]
+for entry in walk:
+	dirs.append(entry[0])
 
 for entry in dirs:
 	print(entry)
