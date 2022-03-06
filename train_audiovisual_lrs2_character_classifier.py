974276f9fa7b6696d717c46284faaa6fd8c51f97
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-03-05 18:59:58 -0700

    Update train_audiovisual_lrs2_character_classifier.py

--- train_audiovisual_lrs2_character_classifier.py
+++ train_audiovisual_lrs2_character_classifier.py
@@ -32,4 +32,6 @@ test_ids = LRS2UnsupervisedLoader(file_path=test_txt_path).load()
 test_set = LRS2AudioVisualCachedCharacterDataset(test_ids, lrs2_path, val_params['batch_size'])
 test_generator = data.DataLoader(test_set, collate_fn=audiovisual_embedding_batch_collate, **val_params)
 
-network = FBAudioVisualCPCCharacterClassifierLightning( batch_size=params['batch_size'], LSTM=True)#src_ckpt, 
+network = FBAudioVisualCPCCharacterClassifierLightning(src_checkpoint_path=None, batch_size=params['batch_size'], LSTM=True) 
+
+trainer = pl.Trainer(gpus=1, callbacks=[EarlyStopping(monitor='val_loss', patience=5)], default_root_dir="lrs2_audiovisual_lstm_character_classifier_lightning_logs")
