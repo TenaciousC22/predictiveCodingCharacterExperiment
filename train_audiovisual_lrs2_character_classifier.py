184d5238b2528cceae55023eae4279b5b9060322
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-03-05 18:36:04 -0700

    Update train_audiovisual_lrs2_character_classifier.py

--- train_audiovisual_lrs2_character_classifier.py
+++ train_audiovisual_lrs2_character_classifier.py
@@ -24,8 +24,10 @@ train_txt_path="/home/analysis/Documents/studentHDD/datasets/LRS2/txts/train.txt
 
 train_ids = LRS2UnsupervisedLoader(file_path=train_txt_path).load()
 training_set = LRS2AudioVisualCachedCharacterDataset(train_ids, lrs2_path, params['batch_size'])
+training_generator = data.DataLoader(training_set, collate_fn=audiovisual_embedding_batch_collate, **params)
 
 test_txt_path="/home/analysis/Documents/studentHDD/datasets/LRS2/txts/val.txt"
 
 test_ids = LRS2UnsupervisedLoader(file_path=test_txt_path).load()
 test_set = LRS2AudioVisualCachedCharacterDataset(test_ids, lrs2_path, val_params['batch_size'])
+test_generator = data.DataLoader(test_set, collate_fn=audiovisual_embedding_batch_collate, **val_params)
