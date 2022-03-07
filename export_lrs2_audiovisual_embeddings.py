ec9fbafb74c8b4e026a0c51a43b72c2b0d141be9
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-03-07 14:36:27 -0700

    Update export_lrs2_audiovisual_embeddings.py

--- export_lrs2_audiovisual_embeddings.py
+++ export_lrs2_audiovisual_embeddings.py
@@ -29,8 +29,8 @@ model.load_state_dict(checkpoint['state_dict'])
 #print(model.cpc_model.gEncoder.conv0.weight)
 
 
-libri_path = "/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1"
-dest_dir = "/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1_embeddings"
+libri_path = "/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1/main"
+dest_dir = "/home/analysis/Documents/studentHDD/datasets/LRS2/mvlrs_v1_embeddings/main"
 
 train_output = True
 
@@ -44,6 +44,8 @@ for folder in os.listdir(libri_path):
                 continue
 
             jobs.append((folder, file))
+
+print(jobs)
 
 for folder, file in tqdm(jobs):
     if not train_output:
