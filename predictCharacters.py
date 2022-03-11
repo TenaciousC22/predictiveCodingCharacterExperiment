c258bf81efd75d39ec0ba634b945f60d89affbbc
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-03-10 20:43:45 -0700

    Update predictCharacters.py

--- predictCharacters.py
+++ predictCharacters.py
@@ -10,6 +10,44 @@ from progressbar import progressbar
 from tqdm import tqdm
 from torch.multiprocessing import Lock, Manager
 from generators.librispeech import LRS2UnsupervisedLoader, LRS2AudioVisualPhonemeDataset
-from models.audiovisual_model import  FBAudioVisualCPCPhonemeClassifierLightning
+from models.audiovisual_model import  FBAudioVisualCPCCharacterClassifierLightning
 from util.pad import audiovisual_batch_collate
 from util.seq_alignment import beam_search
+
+def levenshtein(a, b):
+    "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a, b = b, a
+        n, m = m, n
+
+    current = list(range(n+1))
+    for i in range(1, m+1):
+        previous, current = current, [i]+[0]*n
+        for j in range(1, n+1):
+            add, delete = previous[j]+1, current[j-1]+1
+            change = previous[j-1]
+            if a[j-1] != b[i-1]:
+                change = change + 1
+            current[j] = min(add, delete, change)
+
+    return current[n]
+
+def createDatasetPaths():
+	paths=[]
+
+	for x in range(6):
+		for y in range(28):
+			for key in offsetMap:
+				paths.append("speaker"+str(x+1)+"clip"+str(y+1)+offsetMap[key])
+
+	return paths
+
+#Get a full list of all videos with speakers, sentences, and offsets
+per_ckpt="/home/analysis/Documents/studentHDD/chris/predicitiveCodingCharacterExperiment/lightning_logs/version_3/checkpoints/epoch=6-step=40102.ckpt"
+dest_csv="/home/analysis/Documents/studentHDD/chris/predicitiveCodingCharacterExperiment/predicitiveCodingCharacterResults.csv"
+datasetPath="/home/analysis/Documents/studentHDD/chris/monoSubclips/"
+
+model = FBAudioVisualCPCCharacterClassifierLightning(src_checkpoint_path=per_ckpt, batch_size=1, cached=False, LSTM=True).cuda()
+per_checkpoint = torch.load(per_ckpt)
