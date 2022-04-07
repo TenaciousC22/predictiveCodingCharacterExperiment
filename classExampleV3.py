fba394ea5a8318fe1a28cdef5e95723c38cb7b06
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-04-06 18:54:10 -0600

    I'm dumb

--- classExampleV3.py
+++ classExampleV3.py
@@ -288,7 +288,7 @@ class PositionalEncodingV2(nn.Module):
 	"""
 
 	def __init__(self, dModel, maxLen):
-		super(PositionalEncoding, self).__init__()
+		super(PositionalEncodingV2, self).__init__()
 		pe = torch.zeros(maxLen, dModel)
 		position = torch.arange(0, maxLen, dtype=torch.float).unsqueeze(dim=-1)
 		denominator = torch.exp(torch.arange(0, dModel, 2).float()*(math.log(10000.0)/dModel))
