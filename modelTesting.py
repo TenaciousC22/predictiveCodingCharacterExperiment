24ab1b19ea533c377a7fb2199c43b37d45e82806
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-04-10 11:20:55 -0600

    Update modelTesting.py

--- modelTesting.py
+++ modelTesting.py
@@ -29,10 +29,10 @@ training_generator = data.DataLoader(training_set, collate_fn=audiovisual_embedd
 test_txt_path="/home/analysis/Documents/studentHDD/datasets/LRS2/txts/val.txt"
 
 test_ids = LRS2UnsupervisedLoader(file_path=test_txt_path).load()
-test_set = LRS2AudioVisualCachedCharacterDataset(test_ids, lrs2_path, val_params['batch_size'])
+test_set = LRS2UnsupervisedDataset(test_ids, lrs2_path, val_params['batch_size'])
 test_generator = data.DataLoader(test_set, collate_fn=audiovisual_embedding_batch_collate, **val_params)
 
-network = CPCCharacterClassifierV3(src_checkpoint_path=src_ckpt, batch_size=params['batch_size'], LSTM=True, cached=False)
+network = CPCCharacterClassifierV3(src_checkpoint_path=src_ckpt, batch_size=params['batch_size'], LSTM=False, cached=False)
 
 trainer = pl.Trainer(gpus=1, callbacks=[EarlyStopping(monitor='val_loss', patience=10)], default_root_dir="hybrid_character_classifier_lightning_logs")
 trainer.fit(network, training_generator, test_generator)
