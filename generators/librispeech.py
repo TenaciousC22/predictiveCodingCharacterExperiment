6bc453a69cd94b24ec29ef6c7081647277de685c
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-04-11 10:59:41 -0600

    Update librispeech.py

--- generators/librispeech.py
+++ generators/librispeech.py
@@ -342,7 +342,7 @@ class hybridNetworkDataset():
         X_visual = X_visual_data[randstart_visual:randstart_visual+self.video_sample_len,:].T
 
         #pad for rounding issue
-        X_visual = pad_along_axis(X+visual, self.video_sample_len,  axis=1)
+        X_visual = pad_along_axis(X_visual, self.video_sample_len,  axis=1)
 
         y_data = np.load(os.path.join(self.dataset_dir, ID+'-chars.npy'))
         Y = torch.from_numpy(y_data).int()
