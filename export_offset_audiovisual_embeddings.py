7cd80fe9a675aa5d45d141f4181f4ccfdd85a06e
TenaciousC22 <38121569+TenaciousC22@users.noreply.github.com>
2022-04-13 01:21:48 -0600

    Update export_offset_audiovisual_embeddings.py

--- export_offset_audiovisual_embeddings.py
+++ export_offset_audiovisual_embeddings.py
@@ -83,17 +83,14 @@ train_output = True
 
 jobs,dumb,test=createDatasetPaths()
 
-for entry in jobs:
-	print(entry)
-
 for file in tqdm(jobs):
 	dest = os.path.join(dest_dir, file.replace('.wav', '.npy'))
 
 	# if not os.path.exists(os.path.join(dest_dir, folder)):
 	# 	os.makedirs(os.path.join(dest_dir, folder))
 
-	x_audio, _ = librosa.load(os.path.join(libri_path, folder, file), sr=16000)
-	x_visual = np.load(os.path.join(libri_path, folder, file.replace('.wav', '.npy')))
+	x_audio, _ = librosa.load(os.path.join(libri_path, file), sr=16000)
+	x_visual = np.load(os.path.join(libri_path, file.replace('.wav', '.npy')))
 
 	x_audio_tensor = torch.from_numpy(x_audio).unsqueeze(0).unsqueeze(0).cuda()
 	x_visual_tensor = torch.from_numpy(x_visual.T).unsqueeze(0).cuda()
